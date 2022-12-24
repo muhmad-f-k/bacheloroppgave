8f340eb69b50bc74e50df532d3279cb06d9c4310
Muhmad K. Fauzi <muhmad@live.no>
2022-12-24 01:31:07 +0100

    removed not needed lib stuff

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 from app_pages.home_page import home_page
 from app_pages.contact_page import contact_page
 from app_pages.about_page import about_page
-
-
 
 def main():
   st.sidebar.title("Navigation")
