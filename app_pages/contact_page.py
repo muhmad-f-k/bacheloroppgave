e88bd3f1ef48619da54dc0c8a68a8ca9b7c5ed8e
Muhmad K. Fauzi <muhmad@live.no>
2022-12-24 01:42:00 +0100

    added some info data to the pages

--- app_pages/contact_page.py
+++ app_pages/contact_page.py
@@ -1,4 +1,4 @@
 import streamlit as st
 def contact_page():
-  st.title("Contact Page")
-  st.write("Get in touch with us.")
+  st.title("Get in touch with us.")
+  st.write("Email Address : Muhmad@live.no")
--- app_pages/home_page.py
+++ app_pages/home_page.py
@@ -5,8 +5,8 @@ import random
 data = [[random.randint(0, 100) for _ in range(10)] for _ in range(3)]
 
 def home_page():
-  st.title("Home Page")
-  st.write("Welcome to the home page!")
+  st.title("Bachelor thesis for group 14")
+  st.write("Welcome!")
   st.markdown("Random chart to test the performance")
   # Create the line chart
   st.line_chart(data)
